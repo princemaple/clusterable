510b7a9c113091795eb6ae522728092ed0e55dbf
Po Chen <chenpaul914@gmail.com>
2017-09-21 12:58:29 +1000

    release v0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Clusterable.Mixfile do
 
   def project do
     [app: :clusterable,
-     version: "0.1.1",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
